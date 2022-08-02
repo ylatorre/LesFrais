035846b7ed469070feebb407ccecee5c7dd9cf7c
VincentHeim <heim.vincent@gmail.com>
2022-08-02 10:52:18 +0200

    Ajustement BDD
    réglage de certaines colonnes pour une meilleurs lisibilitée

--- database/migrations/2022_08_02_084419_add_salarie_column_to_users_table.php
+++ database/migrations/2022_08_02_084419_add_salarie_column_to_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('member')->default(1);
             //
+            $table->boolean('salarie')->default(1);
         });
     }
 
--- database/migrations/2022_08_02_084610_add_admin_column_to_users_table.php
+++ database/migrations/2022_08_02_084610_add_admin_column_to_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            //
             $table->boolean('admin')->default(0);
         });
     }
