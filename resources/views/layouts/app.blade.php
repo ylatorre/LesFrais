c3456182d023dd92d6189455a16b0efb5fc358ae
VincentHeim <heim.vincent@gmail.com>
2022-09-06 11:10:15 +0200

    petites modifications concernant la page de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="..\public\images\favicon.ico">
 
-    <title>Notes de frais</title>
+    
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Carpe Diem IT service & Télécom') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="icon" href="..\public\images\favicon.ico">
-        <title>Notes de frais</title>
+        <title>Carpe Diem</title>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
