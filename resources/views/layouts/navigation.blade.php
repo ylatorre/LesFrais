1e3f81fd242fa67319dd3011ce45bb6be2d973e1
VincentHeim <heim.vincent@gmail.com>
2022-09-07 10:58:46 +0200

    petite modification d'interface
    dashboard deviens calendrier

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" style="text-decoration:none">
-                        {{ __('Dashboard') }}
+                        {{ __('Calendrier') }}
                     </x-nav-link>
 
                     @if (auth()->user()->admin)
