6b55f201812847c4a195653620a4e6a01e0bcca0
VincentHeim <heim.vincent@gmail.com>
2022-10-04 15:58:03 +0200

    gestion du cas ou de nombreux  evenement sont créer lke meme jour alors ils sont trier par order alphabétique

--- app/Http/Controllers/PDFgeneratorController.php
+++ app/Http/Controllers/PDFgeneratorController.php
@@ -40,7 +40,7 @@ class PDFgeneratorController extends Controller
 
 
         // - On recupère tous les événements correspondants au mois à l'ecran et au user concerné
-        $utilisateurs = DB::table('users')->RightJoin("events", "events.idUser", "users.id")->where("idUser", "=", $request->idUser)->where('mois','=', $request->selectedMonth)->orderBy("start","asc")->get();
+        $utilisateurs = DB::table('users')->RightJoin("events", "events.idUser", "users.id")->where("idUser", "=", $request->idUser)->where('mois','=', $request->selectedMonth)->orderBy("start","asc")->orderBy("title","asc")->get();
 
 
         if(count($utilisateurs) == 0){
