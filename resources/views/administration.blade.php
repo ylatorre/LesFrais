b79413092057ab5d50eabb9c19070188ac21ebe9
VincentHeim <heim.vincent@gmail.com>
2022-10-12 12:41:20 +0200

    ajustement css modal modifuser

--- resources/views/administration.blade.php
+++ resources/views/administration.blade.php
@@ -545,9 +545,9 @@
 
 
                                         </div>
-                                        
+
                                         <button type="submit"
-                                            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            class="w-full mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Modifier les informations
                                         </button>
 
