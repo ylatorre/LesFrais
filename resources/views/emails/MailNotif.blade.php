45926bdcd2190f242909cfb6e3551e02916b7b2d
VincentHeim <heim.vincent@gmail.com>
2022-11-16 15:23:29 +0100

    modificatino de style du mail

--- resources/views/emails/MailNotif.blade.php
+++ resources/views/emails/MailNotif.blade.php
@@ -21,30 +21,8 @@
             font-size:16px;
             margin-bottom:20px;
         }
-        footer{
-            margin-top:20px;
-        }
-        .table-footer {
-            width: 40%;
-            border: 2px solid #870b26;
-        }
 
-        .table-footer td {
-            text-align: center;
-            height: 76px;
-            font-family: 'nunito', sans-serif;
-            font-weight: bold;
-            color: #870b26;
-        }
 
-        .table-footer th {
-            width: 230px;
-            color: #870b26;
-        }
-        a img{
-            width:24px;
-            height:24px;
-        }
     </style>
 
 
@@ -54,13 +32,5 @@
 
     <p>     L'utilisateur <em>{{$actualUser}}</em> a demandé la validation de sa note de frais de <strong>{{$monthNDF}}.</strong></p>
     <p>     Pour valider cette note de frais, merci de bien vouloir vous connecter à  <a href="https://www.carpediem.pro/LesFrais/public">Carpe Diem</a> afin de la valider dans votre espace Administration.</p>
-
-
-
-
-
-
-
-
 </body>
 </html>
