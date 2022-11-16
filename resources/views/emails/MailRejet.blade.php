ed3ecd47f3d1b95c8cb1b1a06e3f6980b7479c1d
VincentHeim <heim.vincent@gmail.com>
2022-11-16 14:48:25 +0100

    Suppression des signature dans les mails

--- resources/views/emails/MailRejet.blade.php
+++ resources/views/emails/MailRejet.blade.php
@@ -66,29 +66,7 @@
     <div class="footer">
         <p>Très cordialement,</p>
     </div>
-    <div class="signature">
-        <table class="table-footer">
-            <tr>
-                <th><img src="{{ asset('./images/logoCDIT.png') }}" alt="logoCDIT"></th>
-                <th>{{ $rejecter[0]->name }}</th>
-            </tr>
-            <tr>
-                <td style="font-style:italic; height:30px;">assistance@carpediem.pro</td>
-                <td style="font-style:italic; height:30px;">{{ $rejecter[0]->email }}</td>
-            </tr>
 
-            <tr>
-                <td>
-                    <a href="https://www.facebook.com/carpeDiem.itservicestelecom" target="_blank"> <img src="{{ asset('images/iconFacebook.png') }}" alt="fb"></a>
-                    <a href="https://twitter.com/Carpe_Diem_Pro" target="_blank"> <img src="{{ asset('images/Twitter-b@5x.png') }}" alt="tw"></a>
-                    <a href="https://www.linkedin.com/company/carpe-diem-informatique/" target="_blank"> <img src="{{ asset('images/Linkedin-b@5x.png') }}" alt="ln"></a>
-                    <a href="https://www.youtube.com/channel/UCTI2dHTGS9VrwKxGJf5MW7A" target="_blank"> <img src="{{ asset('images/Youtube-b@5x.png') }}" alt="yt"></a>
-                    <a href="https://www.carpediem.pro/" target="_blank"> <img src="{{ asset('images/Link-b@5x.png') }}" alt="link"></a>
-                </td>
-                <td style="font-size: 10px;">LD : 0170.809.809 - Mob : 06.60.67.33.46</td>
-            </tr>
-        </table>
-    </div>
 
 </body>
 </html>
