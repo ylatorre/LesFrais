640ecbefe99c94a849224057c5019374c015c9d6
VincentHeim <heim.vincent@gmail.com>
2022-11-22 11:18:51 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
+            'username' => env('DB_USERNAME','root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
